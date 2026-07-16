95c826c5b81a638773980bee8cf7db300607f847
jrodriver-del <j.rod.river@gmail.com>
2026-07-16 13:38:55 -0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+create database clase1
+go
+
+use clase1
+go
+
+create table usuario(
+id int,
+direc varchar(30),
+telefono int
+)
+go
+
+create table produc(
+id int,not null,
+nombre varchar(20),not null,
+precio int
+)
+go
+
